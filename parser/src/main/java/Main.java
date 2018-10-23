c423e46c7638bab58471f9aadf38726e9c93a95c
awen <awen190418@gmail.com>
2018-10-23 15:46:27 +0900

    Basic Version 1.0.0

--- parser/src/main/java/Main.java
+++ parser/src/main/java/Main.java
@@ -8,6 +8,9 @@ import wf.bitcoin.javabitcoindrpcclient.BitcoindRpcClient;
 import java.lang.reflect.Array;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.sql.Time;
+import java.time.Duration;
+import java.time.Period;
 import java.util.*;
 
 public class Main {
@@ -16,8 +19,6 @@ public class Main {
     public static void main(String args[]) {
         Config c = new Config();
         Main.properties = c.getConfig();
-
-
 
         int port = Integer.valueOf(Main.properties.getProperty("rpcPort"));
         String server = String.valueOf(Main.properties.getProperty("server"));
@@ -29,123 +30,105 @@ public class Main {
         } catch (MalformedURLException e) {
             e.printStackTrace();
         }
+        BitcoinJSONRPCClient RPCclient = new BitcoinJSONRPCClient(url);
 
-
-        BitcoinJSONRPCClient bitcoin = new BitcoinJSONRPCClient(url);
-
+        //Hash of the first block. Block 0 is not parseable therefore not used here in the system.
         String blockHash="00000000839a8e6886ab5951d76f411475428afc90947ee320161bbf18eb6048";
 
-
-
+        //This will sacan the block for always.  just for the while loop.
         while(port>0) {
-            BitcoindRpcClient.Block block = bitcoin.getBlock(blockHash);
+            //1. Block payo
+            BitcoindRpcClient.Block block = RPCclient.getBlock(blockHash);
+            System.out.println(
+                    "\n\n\n\n Block Received"+
+                            "Block Hash: " + blockHash+"\n"+
+                            "Block number: " + block.height()+"\n"
+
+            );
+
+            //2. Block le duni chongu Transaction na paye jula]
             ArrayList<String> TxList = (ArrayList<String>) block.tx();
 
+            //Coinbase Transaction jula la ki output jaka server le cho.
+            BitcoindRpcClient.RawTransaction Tx = RPCclient.getRawTransaction(TxList.get(0));
 
-                //Coinbase Transaction
-                BitcoindRpcClient.RawTransaction Tx = bitcoin.getRawTransaction(TxList.get(0));
+            List<BitcoindRpcClient.RawTransaction.Out> out = (List<BitcoindRpcClient.RawTransaction.Out>) Tx.vOut();
+            //Out ta parse yayu function dayeki mani.
 
-                List<BitcoindRpcClient.RawTransaction.Out> out = (List<BitcoindRpcClient.RawTransaction.Out>) Tx.vOut();
-                List<BitcoindRpcClient.RawTransaction.In> in = (List<BitcoindRpcClient.RawTransaction.In>) Tx.vIn();
+            //Tho IP coinbase kha aaye juya thake ta chu na yaye mwa.
+            List<BitcoindRpcClient.RawTransaction.In> in = (List<BitcoindRpcClient.RawTransaction.In>) Tx.vIn();
 
+            // Input ta parse yagu pati.
 
             if(TxList.size()>1) {
                 for (String TxHash : TxList.subList(1, TxList.size())) {
-                    Tx = bitcoin.getRawTransaction(TxHash);
+                    Tx = RPCclient.getRawTransaction(TxHash);
+                    System.out.println("Transaction No : " + TxList.indexOf(TxHash) );
 
                     out = (List<BitcoindRpcClient.RawTransaction.Out>) Tx.vOut();
                     in = (List<BitcoindRpcClient.RawTransaction.In>) Tx.vIn();
 
+
+                    //Input parsing
                     int i=0;
-
                     while(i<in.size()){
+                        String soruceTXId= in.get(i).txid();
+                        int sourceVoutId= in.get(i).vout();
 
-                        System.out.println(in.get(i).txid()+" "+ String.valueOf(in.get(i).vout()));
-//                        in.get(i).vout();
+
+                        BitcoindRpcClient.RawTransaction sourceTx = RPCclient.getRawTransaction(soruceTXId);
+                        Date receivedDate = sourceTx.blocktime();
+
+                        List<BitcoindRpcClient.RawTransaction.Out> sourceOut = (List<BitcoindRpcClient.RawTransaction.Out>) sourceTx.vOut();
+
+                        String bitcoinAddress = sourceOut.get(sourceVoutId).scriptPubKey().addresses().get(0);
+                        double value = sourceOut.get(sourceVoutId).value();
+                        Date spentDate = block.time();
+
+                        String spentTxId = TxHash;
+                        int spentVinIndex=TxList.indexOf(TxHash);
+
+//                        Duration duration;
+//                        duration = (Duration) Duration.between(spentDate, receivedDate);
+//                        long diff = Math.abs(duration.toMinutes());
+                        double diff = spentDate.getTime() - receivedDate.getTime();
+                        diff = diff / (60 * 60*1000); // minutes
+
+
+                        System.out.println("Input No: " + i);
+                        System.out.println(
+                                "\n\n\n*****************************************************\n"+
+                                "Transaction Details\n"+
+                                "*****************************************************\n"+
+                                "Received TxId: "+soruceTXId+
+                                " \n"+
+                                "ID: "+String.valueOf(sourceVoutId)+
+                                "\n"+
+                                "Received Date: "+ receivedDate.toString()+
+                                " \n\n"+
+
+                                "Spent TxId: "+spentTxId+
+                                " \n"+
+                                "ID: "+String.valueOf(spentVinIndex)+
+                                " \n"+
+                                "Spent Date: "+ spentDate.toString()+
+                                "\n\n"+
+                                "Address: "+bitcoinAddress+
+                                " \n"+
+                                "Amount: "+String.valueOf(value) +
+                                "\n"+
+                                "Holded Duration: " +diff+
+
+                        "\n\n"+
+                                "*****************************************************"
+                        );
                         i++;
                     }
-                    //get the input address and its details
+                    //input parsing ends
                     in = null;
-
-
                 }
             }
-
-
-
-
-
-
-
-
-
-
-
-            blockHash = bitcoin.getBlock(blockHash).nextHash();
-
-
-
-        }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        RPCClient r = new RPCClient();
-
-
-
-
-        ArrayList<String> param = new ArrayList<String>();
-        ArrayList<String> param1 = new ArrayList<String>();
-
-        param.add("000000000019d6689c085ae165831e934ff763ae46a2a6c172b3f1b60a8ce26f");
-
-
-        while (true) {
-            JSONObject obj = r.getInfo("getblock", param);
-            JSONObject obj1 = (JSONObject) obj.get("result");
-            JSONArray obj2 = (JSONArray) obj1.get("tx");
-
-            int i = 0;
-            if(obj2.size()>1){
-                System.out.println("YYYYYY********************************");
-
-            }
-            while (i < obj2.size()) {
-                System.out.println("TX ID---------------> "+obj2.get(i));
-
-                //get the transaction.
-
-                param1.clear();
-                param1.add((String) obj2.get(i) );
-                param1.add("1");
-
-                JSONObject TxDetails = r.getInfo("getrawtransaction", param1);
-
-
-
-
-
-                i++;
-            }
-
-            String nextBlockHash = String.valueOf(obj1.get("nextblockhash"));
-
-
-            param.clear();
-            param.add(nextBlockHash);
+            blockHash = RPCclient.getBlock(blockHash).nextHash();
         }
 
     }
